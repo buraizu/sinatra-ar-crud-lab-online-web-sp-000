7a18b9ba3070eab7879c163ca6476747e0245d71
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-16 06:43:12 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,6 @@
 
 class Post < ActiveRecord::Base
 
-  # attr_accessor :name, :content
-  #
-  # def initialize(params)
-  #   @name = params[:name]
-  #   @content = params[:content]
-  # end
-
 end
 
 
